fb591244701db40d9e32f2cddd7d46e394424e28
hi <zioklsi@naver.com>
2023-08-10 09:17:06 +0900

    포트폴리오 페이지 텍스트 롤링 추가, 소방재난본부 에이펀 메인페이지 완성

--- project1/js/main.js
+++ project1/js/main.js
@@ -1,6 +1,13 @@
 $(function () {
 
+    $(".business").find(".tabTitle li").on("click", function () {
+        $(this).addClass("active");
+        $(this).siblings().removeClass("active");
 
+        const i = $(this).index()
+        $(".business").find(".tabContent").eq(i).addClass("active");
+        $(".business").find(".tabContent").eq(i).siblings().removeClass("active");
+    });
 
 
 
