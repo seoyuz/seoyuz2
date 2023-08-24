2e6eb26095a1d75383b38e037fdd3631d4775fe4
seoyujeong <zioklsi@naver.com>
2023-08-25 00:56:43 +0900

    프로그래스바 수정, 슬라이드 애로우 수정 필요

--- project1/js/work.js
+++ project1/js/work.js
@@ -1,20 +1,23 @@
 $(function () {
+    const $slider = $(".slider");
 
-    $(".slider").on("init", function (event, slick) {
+    $slider.on("init", function (event, slick) {
         const ratio = (slick.currentSlide + 1) / slick.slideCount * 100 + "%"
-        $(".sliderWrap").find(".progressBar").css("width", ratio);
+        $(".slideWrap").find(".progressBar").css("width", ratio);
     })
 
-    $(".slider").slick({
-        centerMode:true,
-        centerPadding:'300px',
-        arrows:true,
+    $slider.slick({
+        centerMode: true,
+        centerPadding: "300px",
+
+        arrows: true,
+        prevArrow: $(".slideWrap .arrowArea").find(".prevArrow"),
+        nextArrow: $(".slideWrap .arrowArea").find(".nextArrow"),
     });
 
-    $(".slider").on("beforeChange", function (event, slick, current, next) {
+    $slider.on("beforeChange", function (event, slick, current, next) {
         const ratio = (next + 1) / slick.slideCount * 100 + "%"
-        $(".sliderWrap").find(".progressBar").css("width", ratio);
-    })
-
+        $(".slideWrap").find(".progressBar").css("width", ratio);
+    });
 
 });
