fd42f06de0e3dafe3cf14d50874eb9a504350d8b
seoyujeong <zioklsi@naver.com>
2023-08-25 18:22:38 +0900

    work 페이지 슬라이드 오류 수정

--- project1/js/work.js
+++ project1/js/work.js
@@ -1,23 +1,115 @@
 $(function () {
-    const $slider = $(".slider");
+    // const $slider = $(".slider");
+    //
+    // $slider.on("init", function (event, slick) {
+    //     let index = $(this).index();
+    //     const ratio = (slick.currentSlide + 1) / slick.slideCount * 100 + "%"
+    //     $(".progressBar").eq(index).css("width", ratio);
+    // });
+    //
+    // $slider.slick({
+    //     centerMode: true,
+    //     centerPadding: "300px",
+    //     arrows: false,
+    //
+    //     // autoplay: true
+    // });
+    //
+    // $slider.on("beforeChange", function (event, slick, current, next) {
+    //     let index = $(this).index();
+    //     const ratio = (next + 1) / slick.slideCount * 100 + "%"
+    //     $(".progressBar").eq(index).css("width", ratio);
+    //
+    //     //첫슬라이드 버그수정 코드//
+    //     if (current !== next) {
+    //         $(".slick-current + .slick-cloned").each(function(i, v) {
+    //             const item = $(v);
+    //             setTimeout(function() {
+    //                 item.addClass("slick-current");
+    //                 item.addClass("slick-active");
+    //             });
+    //         });
+    //     }
+    // });
 
-    $slider.on("init", function (event, slick) {
-        const ratio = (slick.currentSlide + 1) / slick.slideCount * 100 + "%"
-        $(".slideWrap").find(".progressBar").css("width", ratio);
-    })
 
-    $slider.slick({
-        centerMode: true,
-        centerPadding: "300px",
+    // $(".work2023").on("init", function (event, slick) {
+    //     const ratio = (slick.currentSlide + 1) / slick.slideCount * 100 + "%"
+    //     $(this).siblings().find(".progressBar").css("width", ratio);
+    // });
+    // $(".work2023").slick({
+    //     centerMode: true,
+    //     centerPadding: '300px',
+    //
+    //     arrows: true,
+    //     appendArrows: true,
+    //     prevArrow: $(this).siblings().find(".prevArrow"),
+    //     nextArrow: $(this).siblings().find(".nextArrow"),
+    // });
+    // $(".work2023").on("beforeChange", function (event, slick, current, next) {
+    //     const ratio = (next + 1) / (slick.slideCount) * 100 + "%"
+    //     $(this).siblings().find(".progressBar").css("width", ratio);
+    // });
+    //
+    // $(".work2022").on("init", function (event, slick) {
+    //     const ratio = (slick.currentSlide + 1) / slick.slideCount * 100 + "%"
+    //     $(this).siblings().find(".progressBar").css("width", ratio);
+    // });
+    // $(".work2022").slick({
+    //     centerMode: true,
+    //     centerPadding: '300px',
+    //
+    //     arrows: true,
+    //     appendArrows: true,
+    //     prevArrow: $(this).siblings().find(".prevArrow"),
+    //     nextArrow: $(this).siblings().find(".nextArrow"),
+    // });
+    // $(".work2022").on("beforeChange", function (event, slick, current, next) {
+    //     const ratio = (next + 1) / (slick.slideCount) * 100 + "%"
+    //     $(this).siblings(".progressArea").find(".progressBar").css("width", ratio);
+    // });
+    //
+    // $(".work2021").on("init", function (event, slick) {
+    //     const ratio = (slick.currentSlide + 1) / slick.slideCount * 100 + "%"
+    //     $(this).siblings(".progressArea").find(".progressBar").css("width", ratio);
+    // });
+    // $(".work2021").slick({
+    //     centerMode: true,
+    //     centerPadding: '300px',
+    //
+    //     arrows: true,
+    //     appendArrows: true,
+    //     prevArrow: $(this).siblings().find(".prevArrow"),
+    //     nextArrow: $(this).siblings().find(".nextArrow"),
+    // });
+    // $(".work2021").on("beforeChange", function (event, slick, current, next) {
+    //     const ratio = (next + 1) / (slick.slideCount) * 100 + "%"
+    //     $(this).siblings(".progressArea").find(".progressBar").css("width", ratio);
+    // });
 
-        arrows: true,
-        prevArrow: $(".slideWrap .arrowArea").find(".prevArrow"),
-        nextArrow: $(".slideWrap .arrowArea").find(".nextArrow"),
+
+
+    $(".slider").each(function () {
+        const $progressBar = $(this).parent().find(".progressBar");
+
+        $(this).on("init", function (event, slick) {
+            const ratio = (slick.currentSlide + 1) / slick.slideCount * 100 + "%";
+            $progressBar.css("width", ratio);
+        });
+
+        $(this).on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+            const ratio = (nextSlide + 1) / slick.slideCount * 100 + "%";
+            $progressBar.css("width", ratio);
+        });
+
+        $(this).slick({
+            centerMode: true,
+            centerPadding: '300px',
+
+            prevArrow: $(this).parent().find(".prevArrow"),
+            nextArrow: $(this).parent().find(".nextArrow")
+        });
     });
 
-    $slider.on("beforeChange", function (event, slick, current, next) {
-        const ratio = (next + 1) / slick.slideCount * 100 + "%"
-        $(".slideWrap").find(".progressBar").css("width", ratio);
-    });
 
 });
