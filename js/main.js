c27674c20122eca9042b7ffe65af168ec85e1cd2
seoyujeong <zioklsi@naver.com>
2023-09-04 17:32:37 +0900

    카카오톡 qr코드 추가, 다크모드시 프로젝트 슬라이드 desc 색상 수정 완료

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 
 $(function() {
-    $(window).on("scroll", function() {
-        $(this).scrollTop();
-    });
 
+/*dark mode switch*/
     $(".toggleButton").on("click", function () {
        $("main").toggleClass("dark");
     });
 
+/*follow cursor*/
     function mousemoveEvent(){
         let $win = $(window);
         const $cursor = $(".followCursor");
@@ -28,59 +27,19 @@ $(function() {
     }
     mousemoveEvent();
 
-    // gsap.registerPlugin(ScrollTrigger);
-    //
-    // gsap.to(".textScrolling .textLeft",
-    //     {
-    //         scrollTrigger: {
-    //             trigger: ".textScrolling",
-    //             top: "top top",
-    //             end: "bottom bottom",
-    //             pin: true,
-    //             markers: true,
-    //             scrub: 3,
-    //         },
-    //         x: 300
-    //     }
-    // );
-    //
-    // gsap.to(".textScrolling .textRight",
-    //     {
-    //         scrollTrigger: {
-    //             trigger: ".textScrolling",
-    //             top: "top top",
-    //             end: "bottom bottom",
-    //             pin: true,
-    //             markers: true,
-    //             scrub: 3,
-    //         },
-    //         x: -300
-    //     }
-    // );
-
-
-
+/*#intro mouse hover event*/
     $(".intro .introTxt-3").find("span").on("mouseenter", function() {
         $(".intro .introduceArea .introTxt-3 .introImg").css("opacity", "1");
     }).on("mouseleave", function() {
         $(".intro .introduceArea .introTxt-3 .introImg").css("opacity", "");
     });
 
-
+/*header*/
     $(window).on("scroll", function() {
         // $(this).scrollTop(); // 스크롤 좌표를 px 단위로 알려준다.
         scrollTop = $(this).scrollTop();
         scrollHeaderEvent();
-
-        //텍스트 스크롤 어케하는거임 ㅜ
-        //  $(".textScrolling").find(".textLeft").css("left", Math.max(500 - 0.35 * $(this).scrollY) + "px");
-
-        // $(".textScrolling").find(".textLeft").css({
-        //     transform: `translateX(${scrollTop}px)`,
-        //    // left: scrollTop + "px",
-        // });
     });
-
 
     let scrollTop = 0;
     function scrollHeaderEvent() {
@@ -97,7 +56,8 @@ $(function() {
     }
     scrollHeaderEvent();
 
-const $projectSlider = $("#projectSlider");
+/*project section slide*/
+    const $projectSlider = $("#projectSlider");
 
     $projectSlider.on("init", function (event, slick) {
         $(".textBoxWrap").find(".textBox").eq(slick.currentSlide).addClass("active");
@@ -132,6 +92,13 @@ const $projectSlider = $("#projectSlider");
         $(".project").find(".progressBar").css("width", ratio);
     });
 
+/*contact section popup*/
+    $(".kakaoQr").on("click",function () {
+        $(".contact").find(".imgPopup").css("opacity", "1");
+    });
+    $(".imgPopup .closeArea").on("click",function () {
+        $(".contact").find(".imgPopup").css("opacity", "0");
+    });
 
 });
 
